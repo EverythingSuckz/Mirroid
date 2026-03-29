bd2e74ed222c45f54f20656e84bc4dbe5624d370
Jyothis <me@wrench.rocks>
2026-03-29 15:52:45 -0400

    fix: escape single quotes in clipboard PowerShell command to prevent injection

--- internal/platform/clipboard_windows.go
+++ internal/platform/clipboard_windows.go
@@ -2,12 +2,16 @@
 
 package platform
 
-import "os/exec"
+import (
+	"os/exec"
+	"strings"
+)
 
 // CopyImageToClipboard copies an image file to the system clipboard using PowerShell.
 func CopyImageToClipboard(filePath string) error {
+	escapedPath := strings.ReplaceAll(filePath, "'", "''")
 	psCmd := exec.Command("powershell", "-NoProfile", "-Command",
-		`Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetImage([System.Drawing.Image]::FromFile('`+filePath+`'))`)
+		`Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Clipboard]::SetImage([System.Drawing.Image]::FromFile('`+escapedPath+`'))`)
 	HideConsole(psCmd)
 	return psCmd.Run()
 }
